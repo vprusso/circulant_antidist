f525ebbe329fe936d8a2a9bf3b7c1dfee54f17b0
vrusso <vincentrusso1@gmail.com>
2023-07-16 09:34:37 -0400

    Adding more examples.

--- examples/random_antidist.py
+++ examples/random_antidist.py
@@ -7,5 +7,6 @@ from toqito.state_opt import state_exclusion
 num_examples, num_states, dim = 10, 4, 4
 for _ in range(num_examples):
     vectors = [random_statevector(dim).data for _ in range(num_states)]  
-    print(f"Is antidistinguishable: {}") 
-    
+    opt_val, _ = state_exclusion(vectors, primal_dual="dual")
+    print(f"Is antidistinguishable: {np.isclose(0, opt_val)}") 
+ 
